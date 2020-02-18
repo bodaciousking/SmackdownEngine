b3fcc6f85479ef8841c6646da4fd47350eb8e2f6
TheLampost <34274706+TheLampost@users.noreply.github.com>
2020-02-18 13:27:04 -0500

    Audio Component
    Working !

---
+++ SmackdownEngine/AudioComponent.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <SFML/Audio.hpp>
+class AudioComponent
+{
+public:
+	AudioComponent();
+	~AudioComponent();
+	static void Initialize();
+	static void PlayMusic(std::string filename);
+	static void StopMusic();
+private:
+	static sf::SoundBuffer buffer;
+	static sf::Music music;
+	static sf::Sound sound;
+};
+
